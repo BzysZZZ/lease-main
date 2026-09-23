e4388aed9fbb801d98a289757557859aabb400ac
pjj <1971294335@qq.com>
2026-09-23 23:51:51 +0800

    feat(数据库): 1

--- web/web-admin/src/main/java/com/qianlou/admin/controller/login/LoginController.java
+++ web/web-admin/src/main/java/com/qianlou/admin/controller/login/LoginController.java
@@ -38,6 +38,7 @@ public class LoginController {
     @Operation(summary = "获取登陆用户个人信息")
     @GetMapping("info")
     public Result<SystemUserInfoVo> info() {
+
         SystemUserInfoVo user = loginService.getSystemUserInfoById(LoginUserContext.getLoginUser().getUserId());
         return Result.ok(user);
     }
